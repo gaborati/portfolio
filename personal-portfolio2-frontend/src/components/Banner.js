02c23219c0400aba35b847da8dc64c6b74eaceab
gaborati <113521931+gaborati@users.noreply.github.com>
2024-03-18 18:56:05 +0100

    modified frontend astronaut svg

--- personal-portfolio2-frontend/src/components/Banner.js
+++ personal-portfolio2-frontend/src/components/Banner.js
@@ -27,7 +27,7 @@ export const Banner = () => {
                         <Col xs={12} md={6} xl={5}>
                             <TrackVisibility>
                                 {({ isVisible }) =>
-                                    <div >
+                                    <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
                                         <img src={headerImg} alt="Header Img"/>
                                     </div>}
                             </TrackVisibility>
